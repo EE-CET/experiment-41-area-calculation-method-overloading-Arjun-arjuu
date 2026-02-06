b542cbf373c8add2e3641ee8f0c46acc4d5cfd55
ARJUN M <arjunmurali091@gmail.com>
2026-02-06 11:00:46 +0530

    code submitted

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
 public class AreaCalculator {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int r=sc.nextInt();
-        int l=sc.nextInt();
-        int b=sc.nextInt();
-        int ba=sc.nextInt();
-        int h=sc.nextInt();
-        double carea=Math.PI*r*r;
-        double rarea=l*b;
-        double tarea=0.5*ba*h;
-        System.out.println(String.format("%.2f",carea));
-        System.out.println(String.format("%.2f",rarea));
-        System.out.println(String.format("%.2f",tarea));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        double r = sc.nextDouble();  // Radius
+        double l = sc.nextDouble();  // Length
+        double b = sc.nextDouble();  // Breadth
+        double ba = sc.nextDouble(); // Base
+        double h = sc.nextDouble();  // Height
+
+        double carea = Math.PI * r * r;
+        double rarea = l * b;
+        double tarea = 0.5 * ba * h;
+        System.out.printf("%.2f%n", carea);
+        System.out.printf("%.2f%n", rarea);
+        System.out.printf("%.2f%n", tarea);
+        
+        sc.close();
     }
 }
